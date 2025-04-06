bd5299f2598ba4c3ca2022d628c2e61e7170fe9a
Julian Cutaya <julian@mac.home>
2025-04-06 11:10:48 +0200

    init

---
+++ src/produit/Sanglier.java
@@ -0,0 +1,19 @@
+package produit;
+
+import personnages.Gaulois;
+
+public class Sanglier extends Produit {
+	private double poids;
+	private Gaulois chasseur;
+	
+	public Sanglier(double poids, Gaulois chasseur) {
+		super("sanglier",Unite.KILOGRAMME);
+		this.poids = poids;
+		this.chasseur = chasseur;
+	}
+	
+	@Override
+	public String decrireProduit() {
+		return getNom() + " de " + this.poids + unite.toString() + " chassé par " + chasseur.getNom();
+	}
+}
---
+++ src/produit/Unite.java
@@ -0,0 +1,19 @@
+package produit;
+
+
+public enum Unite {
+	
+
+	GRAMME("g"), KILOGRAMME("kg"), LITRE("l"), CENTILITRE("cl"), MILLILITRE("ml"), PIECE("piece");
+	
+	private String nom;
+	
+	private Unite(String nom) {
+		this.nom = nom;
+	}
+	
+	public String toString() {
+		return this.nom;
+	}
+	
+}
